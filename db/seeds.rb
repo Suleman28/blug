069d5232ff40d16034d9cd068202a40f165a604f
Suleman28 <20860617+Suleman28@users.noreply.github.com>
2022-08-28 16:06:27 +0100

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(username: "Suleman", email: "hello@suleman.fun")
+User.create(username: "Suleman", email: "hello@suleman.fun", password: "password")
