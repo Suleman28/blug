8bd021da8cd37154d17387621ec80f11ed99cd0c
Suleman28 <20860617+Suleman28@users.noreply.github.com>
2022-08-28 16:03:01 +0100

    Added missing gems and fixed a few bugs

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
     belongs_to :user
     validates :title, presence: true, length: { minimum: 10, maximum: 100 }
     validates :description, presence: true, length: { minimum: 20 }
-    validates :author, presence: true, length: { minimum: 3, maximum: 100 }
+    validates :author, presence: false, length: {  maximum: 100 }
 end
